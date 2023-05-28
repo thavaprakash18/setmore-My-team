ec2a5721adff9d4677f6fd7ecc3282b0fed5c74d
thavaprakash18 <122508700+thavaprakash18@users.noreply.github.com>
2023-05-28 18:24:25 +0530

    Update loginPageLocators.js

--- tests/setmore/Locators/loginPageLocators.js
+++ tests/setmore/Locators/loginPageLocators.js
@@ -2,7 +2,6 @@ const loginPageLocators = {
     EmailIDInputBox : 'xpath=(.//input[@class="email-field"])',
     passwordInputBox : 'xpath=(.//input[@id="password"])',
     loginButton : 'xpath=(.//a[@id="login-now"])'
-    
 }       
 exports.EmailIDInputBox=loginPageLocators.EmailIDInputBox;
 exports.passwordInputBox=loginPageLocators.passwordInputBox;
