d0e385218ced9aba2c68cd01f95fedb8a1f52d16
thavaprakash18 <122508700+thavaprakash18@users.noreply.github.com>
2023-05-28 18:23:03 +0530

    Update loginPage.js

--- tests/setmore/Pages/loginPage.js
+++ tests/setmore/Pages/loginPage.js
@@ -2,22 +2,14 @@ const{username,loginURl,password,calendarUrl}=require("../Data/myTeamData")
 const{EmailIDInputBox,passwordInputBox,loginButton}=require("../Locators/loginPageLocators")
 
 exports.setmoreLoginPage = class setmoreLoginPage {
-
-  
   constructor(page) {
     this.page = page;
- 
   }
-
   async login() {
     await this.page.goto(loginURl);
     await this.page.locator(EmailIDInputBox).fill(username);
     await this.page.locator(passwordInputBox).fill(password);
     await this.page.locator(loginButton).click();
     await this.page.waitForURL(calendarUrl)
-   
   }
-
-  
-  
 }
